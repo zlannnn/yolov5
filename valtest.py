c67f7e4bbfddc337b189679bc3b4793f372dcf2e
zlannnn <15801823611@163.com>
2022-08-09 10:51:16 +0800

    Update valtest.py

--- valtest.py
+++ valtest.py
@@ -347,7 +347,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default=ROOT / 'data/roadsign_voc.yaml', help='dataset.yaml path')
     parser.add_argument('--weights', nargs='+', type=str, default='/home/kylin/work/dataset_yolov5/MYD_Compressor_EvalPipe_Yolov5_V1.0.1.0/yolov5_16x_mAP0_5_img640_0.941.pt', help='model.pt path(s)')
-    parser.add_argument('--batch-size', type=int, default=32, help='batch size')
+    parser.add_argument('--batch-size', type=int, default=1, help='batch size')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.6, help='NMS IoU threshold')

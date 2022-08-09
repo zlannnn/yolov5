6fe938c6b8c8630d9f0d6625f35ba6a2dd46d090
zlannnn <15801823611@163.com>
2022-08-09 10:48:12 +0800

    Update valtest.py

--- valtest.py
+++ valtest.py
@@ -192,7 +192,7 @@ def run(
     callbacks.run('on_val_start')
     pbar = tqdm(dataloader, desc=s, bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}')  # progress bar
     
-    yolo_path = '/usr/lib/libmfmodeltesterv2.so'
+    yolo_path = '/home/kylin/deploy/mfmodeltesterv2/install/kylin/lib/libmfmodeltesterv2.so'
     mf_yolo = MFTesterC.MFTesterC(yolo_path)
     print('load library finished!')
     yaml_path = '/home/kylin/work/dataset_yolov5/CCSFF_yolov5#N1#640_sparseX16_int8_single_core_fase_b2_mode_false_context1_batch_parallel_false_0/CCSFF_yolov5#N1#640_sparseX16_int8_single_core_fase_b2_mode_false_context1_batch_parallel_false_0_7a6a82d/chip_runtime.yaml'

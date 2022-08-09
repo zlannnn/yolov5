f6bbafd8426dd685e1cb7f68a51ca343772c7760
zlannnn <15801823611@163.com>
2022-08-09 16:10:19 +0800

    Update valtest.py

--- valtest.py
+++ valtest.py
@@ -212,13 +212,11 @@ def run(
         dt[0] += t2 - t1
         # Inference
         #out, train_out = model(im) if training else model(im, augment=augment, val=True)  # inference, loss outputs
-
+        import pdb; pdb.set_trace()
         #im = im.permute(0,2,3,1
         print(im.shape)
         im = im.numpy()
-        im = np.transpose(im,(0,2,3,1))
-        im = np.ascontiguousarray(im, dtype=np.uint8)
-        print(im.shape)       
+        #im = np.ascontiguousarray(im, dtype=np.uint8)     
         im = im.view(dtype=np.uint8)
         print(im.shape)
         mf_yolo.append_param(im, im.nbytes)

47149db6742efd3aa5d94261680d96518de8286a
zlannnn <15801823611@163.com>
2022-08-09 18:04:11 +0800

    Update yolotest.py

--- yolotest.py
+++ yolotest.py
@@ -8,7 +8,7 @@ print('load library finished!')
 yaml_path = '/home/kylin/work/dataset_yolov5/CCSFF_yolov5#N1#640_sparseX16_int8_single_core_fase_b2_mode_false_context1_batch_parallel_false_0/CCSFF_yolov5#N1#640_sparseX16_int8_single_core_fase_b2_mode_false_context1_batch_parallel_false_0_7a6a82d/chip_runtime.yaml'
 ret = mf_yolo.init(yaml_path.encode('utf-8'), 0)
 myout = np.ones([1,1,1,1,25200,32], dtype=np.uint8)
-
+import pdb; pdb.set_trace()
 im = torch.randn(1,640,640,3)
 im = im.numpy().view(dtype=np.uint8)
 print(im.shape)
